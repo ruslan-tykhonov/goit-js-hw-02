8a1d4bf2135f971e16424911cecf13c801b35a3e
ruslan-tykhonov <RUSLANEGOROVICH1943@GMAIL.COM>
2024-10-07 19:10:44 +0200

    change

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,14 @@
 function checkForSpam(message) {
-  // Перетворюємо рядок на нижній регістр для перевірки незалежно від регістру символів
-  const lowerCaseMessage = message.toLowerCase();
-  
-  // Перевіряємо, чи містить рядок заборонені слова "spam" або "sale"
-  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+  return (
+    message.toLowerCase().includes(`spam`) ||
+    message.toLowerCase().includes(`sale`)
+  );
 }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
